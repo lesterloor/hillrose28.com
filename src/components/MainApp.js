6e8f981126c0ef1e7309b803e4f67e4c63810b50
lesterloor <lester@vurteau.com>
2019-08-26 14:25:06 -0400

    no message

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import '../styles/layout/layout.scss'
-import HomeNav from "../components/Navigation"
+import Navigation from "../components/Navigation"
 
 class Dashboard extends Component {
-
     render() {
         const { children } = this.props
         return (
             <div className="page-layout">
-                <HomeNav />
+                <Navigation />
                 {children}
             </div>
         )
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,52 +1,36 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-// import $ from 'jquery';
 import { NavLink } from 'react-router-dom'
-// import logo from "../assets/images//Hillrose_logo_white.png"
-import "../styles/layout/navigation.scss"
-class HomeNavigation extends Component {
-    constructor() {
-        super()
-        this.state = {
-            menuOpened: false
-        }
-    }
-    toggleMenu = () => {
-        // var isOpened = $("#burger-2").hasClass("clicked")
-        // isOpened ? helpers.fadeOutNav() : helpers.fadeInNav()
-    }
-    componentDidMount = () => {
+import ReactSVG from 'react-svg'
+import Logo from "../assets/images/SVG/logo.svg"
 
-    }
+import "../styles/layout/navigation.scss"
+class Navigation extends Component {
+
     render() {
 
         return (
-            <div id="nav-wrapper" className="white-nav">
-                <div id="burger-container" onClick={this.toggleMenu}>
-                    <div id="burger-1" className='burger'></div>
-                    <div id="burger-2" className='burger'></div>
-                    <div id="burger-3" className='burger'></div>
+            <div className="header">
+                <div className="header-menu">
+                    <input class="toggle" id="toggle" type="checkbox" />
+                    <label class="toggle-label" for="toggle"><span class="toggle-icon"></span><span class="toggle-text"></span></label>
                 </div>
-                <div className="nav-item-container">
-                    <NavLink id="items" to="/" className="menu-item">Building</NavLink>
-                    <NavLink id="items" to="/residences" className="menu-item">Residences</NavLink>
-                    <NavLink id="items" to="/penthouses" className="menu-item">Penthouses</NavLink>
-                    <NavLink id="items" to="/amenities" className="menu-item">Amenities</NavLink>
-                    <NavLink id="items" to="/neighborhood" className="menu-item">Neighborhood</NavLink>
-                    <NavLink id="items" to="/availability" className="menu-item">Availability</NavLink>
-                    <NavLink id="items" to="/team" className="menu-item">Team</NavLink>
-                    <NavLink id="items" to="/press" className="menu-item">Press</NavLink>
-                    <NavLink id="items" to="/contact" className="menu-item">Contact</NavLink>
-
-                    <div id="logo-container">
-                        <div className="logo-text">
-                            <p><span>181</span></p>
-                            <p><span>HILLROSE</span>28</p>
-                        </div>
+                <div className="header-nav">
+                    <NavLink id="items" to="/" className="nav-item">Building</NavLink>
+                    <NavLink id="items" to="/residences" className="nav-item">Residences</NavLink>
+                    <NavLink id="items" to="/penthouses" className="nav-item">Penthouses</NavLink>
+                    <NavLink id="items" to="/amenities" className="nav-item">Amenities</NavLink>
+                    <NavLink id="items" to="/neighborhood" className="nav-item">Neighborhood</NavLink>
+                    <NavLink id="items" to="/availability" className="nav-item">Availability</NavLink>
+                    <NavLink id="items" to="/team" className="nav-item">Team</NavLink>
+                    <NavLink id="items" to="/press" className="nav-item">Press</NavLink>
+                    <NavLink id="items" to="/contact" className="nav-item">Contact</NavLink>
+                    <div className="header-logo">
+                        <ReactSVG className="logo" src={Logo} />
                     </div>
-                </div>
 
+                </div>
             </div>
         )
     }
@@ -59,4 +43,4 @@ const mapDispatchToProps = dispatch => bindActionCreators({
     // toggleGlobalSidebar
 }, dispatch)
 
-export default connect(mapStateToProps, mapDispatchToProps)(HomeNavigation)
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation)
